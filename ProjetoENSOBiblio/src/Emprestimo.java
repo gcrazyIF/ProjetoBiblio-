9f608d10fd71c8cfa2a4c66a104b119f5ac10501
gcrazyIF <gcs22@aluno.ifal.edu.br>
2025-02-07 16:36:59 -0300

    começando a interface

--- ProjetoENSOBiblio/src/Emprestimo.java
+++ ProjetoENSOBiblio/src/Emprestimo.java
@@ -1,9 +1,20 @@
 import java.util.*;
-import java.time.LocalDate;
 public class Emprestimo {
     String IDEmprestimo, dataVencimento, dataNovaVencimento;
     boolean emprestado, reservado;
+    Scanner lerEmprestimo = new Scanner(System.in);
 
+    public Emprestimo(){
+        System.out.println("Insira o ID do Empréstimo: ");
+        IDEmprestimo = lerEmprestimo.next();
+        
+        System.out.println("Insira a data de vencimento: ");
+        dataVencimento = lerEmprestimo.next();
+        
+        emprestado = true;
+        reservado = false;
+    }
+    
     public boolean isEmprestado() {
         return emprestado;
     }
